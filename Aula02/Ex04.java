af0d8c49f1e15f8060175ddd43c0f50923ba5210
LucasGFeldmann <lucasgfeldmann@gmail.com>
2024-08-14 22:23:33 -0300

    2ª Aula

---
+++ Aula02/Ex04.java
@@ -0,0 +1,12 @@
+public class Ex04 {
+    public static void main(String[] args) {
+        for (int x = 1; x < 1000000; x++) {
+            int total = 0;
+            for (int y = 1; y < x; y++)
+                if (x % y == 0) {
+                    total += y;
+                }
+            if (x == total) System.out.println(x);
+        }
+    }
+}
